06160dc7b52dc7ec08d8707628aebda54962172e
Priscila Lins <priscila_alins@hotmail.com>
2025-10-16 12:02:44 -0300

    :sparkles: Finalização da Controller Produto

--- src/main/java/com/techbloom/delivery/controller/ProdutoController.java
+++ src/main/java/com/techbloom/delivery/controller/ProdutoController.java
@@ -1,17 +1,27 @@
 package com.techbloom.delivery.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.techbloom.delivery.model.Produto;
 import com.techbloom.delivery.repository.ProdutoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
@@ -39,5 +49,31 @@ public class ProdutoController {
 	    }
 	
 	
-
+	@PostMapping // criar produto
+	public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto){
+		produto.setId(null);
+		return ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto));
+	}
+	
+	@PutMapping // atualizar produto
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto){
+        return produtoRepository.findById(produto.getId())
+            .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto)))
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+	
+	// deletar produto
+	@ResponseStatus(HttpStatus.NO_CONTENT) 
+	@DeleteMapping ("/{id}") 
+	public void delete(@PathVariable Long id) {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		if(produto.isEmpty()) 
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		produtoRepository.deleteById(id); 
+	}
+	
+	
+	
+	
 }
+
